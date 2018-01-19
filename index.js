590daf563fc818d2374fa5a990c428f1255878c7
Sophie Kane <skane0207@gmail.com>
2018-01-19 22:39:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function iLoveTheBeatles(num) {
   do {
     array.push("I love the Beatles!") 
   }
-  while   (num < 15) ;
+  while   (num < 15 && iLoveTheBeatles()) ;
     
     return array;
   }
